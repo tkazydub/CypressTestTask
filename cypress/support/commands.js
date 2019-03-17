a62003ffd62210b94584362b2735433c04fffba3
Taras Kazydub <taras.kazydub@pb.com>
2019-03-17 13:44:37 +0200

    removing screenshots

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,20 @@
-import {MEMBERS, COLUMNS} from "../constants/fields";
+/**
+ * This file contains basic interractions with web elements
+ */
 
+ /**
+  *  @param {string} field - input field locator
+  *  @param {string} value - value to enter at input field
+  */
 Cypress.Commands.add("fillWith", (field, value) => {
-    cy.get(field).clear().type(value)
-})
-
+    cy.get(field).clear().type(value);
+});
+/**
+  *  @param {string} element - element locator to perfrom click at
+  *  @param {string} parent - optional param, to specify parent element
+  */
 Cypress.Commands.add("clickElement", (element, parent=undefined) => {
     parent ? 
     parent.find(element).click() :
     cy.get(element).click();
-})
-
-
-Cypress.Commands.add("moveMember", (fullName, direction) => {
-    cy.contains(MEMBERS.MEMBER_FULL_NAME, fullName).then(res => {
-        res.closest(MEMBERS.CONTAINER).find(direction).click();
-    });
 });
